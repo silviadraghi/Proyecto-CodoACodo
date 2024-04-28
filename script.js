e8c496d40c9aac20c7ebd1f8ca513c1d4bb39578
ivandveliz@gmail.com <ivandveliz@gmail.com>
2024-04-28 14:35:08 -0300

    mediaquerys finalizadas

--- script.js
+++ script.js
@@ -3,8 +3,7 @@
 const sliderContainer = document.querySelector('.slider');
 const sliderImages = document.querySelectorAll('.imagen-slider');
 const totalImages = sliderImages.length;
-
-
+ 
 let currentIndex = 0;
 
 
@@ -20,10 +19,26 @@ function showNextImage() {
   
   function startTimer() {
      sliderImages[currentIndex].style.display = 'block';
-  
-    setInterval(showNextImage, 5000);
+      setInterval(showNextImage, 5000);
   }
   
   window.addEventListener('load', startTimer);
   
-  
+   
+const navBar = document.querySelector("#nav-bar");
+const botonCerrar =document.querySelector("#boton-cerrar");
+const botonesBarra = document.querySelectorAll("#item-link")
+const buttonNavBarOpen = document.querySelector('#btn-nav-bar')
+
+buttonNavBarOpen.addEventListener('click', function () {
+    navBar.classList.add('nav-active')
+  })
+botonCerrar.addEventListener("click", () =>{
+    navBar.classList.remove("nav-active")
+})
+
+botonesBarra.forEach((boton) =>{
+  boton.addEventListener("click", () =>{
+    navBar.classList.remove("nav-active")
+  })
+})
